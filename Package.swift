b739db5b85c396f17bce2df7be6fa2c7bc7280f9
zhouxingfa <zhxf2005307@163.com>
2023-10-24 15:49:00 +0800

    提交spm兼容方案的初始配置

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BPenPlusSDKSPM",
+    platforms: [.iOS(.v12)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "BPenPlusSDKSPM",
+            type: .dynamic,
+            targets: ["BPenPlusSDKSPM"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/zhxf2012/BPBleOTA", from: "0.6.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        
+        .target(
+                    name: "BPenPlusSDKSPM",
+                    dependencies: ["BPenPlusSDKInner",
+                                   .product(name: "BPBleOTA",package: "BPBleOTA")]
+                ),
+        .binaryTarget(
+            name: "BPenPlusSDKInner",
+            path: "BPenPlusSDK/BPenPlusSDK.xcframework"
+        ),
+        .testTarget(
+            name: "BPenPlusSDKSPMTests",
+            dependencies: ["BPenPlusSDKSPM"]),
+    ]
+)
---
+++ Sources/BPenPlusSDKSPM/BPenPlusSDKSPM.swift
@@ -0,0 +1,9 @@
+import BPenPlusSDK
+
+public struct BPenPlusSDKSPMService {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+        
+    }
+}
