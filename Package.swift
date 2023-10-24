51b8cfcb8f35f643433edb43032b3e74bf04dd1b
zhouxingfa <zhxf2005307@163.com>
2023-10-24 16:52:20 +0800

    修正一处警告

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "BPenPlusSDKSPM",
-            type: .dynamic,
+           // type: .dynamic,
             targets: ["BPenPlusSDKSPM"]),
     ],
     dependencies: [
